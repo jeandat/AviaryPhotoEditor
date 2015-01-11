39e8caa229f80ddf70bee75d4bdce70470b75f53
jan4dev <jan4dev@gmail.com>
2015-01-11 17:06:22 +0100

    Added a standard MacOSX native menu.

--- boot.js
+++ boot.js
@@ -1,1 +1,7 @@
 console.info('Hello, I\'m a node-webkit and Aviary SDK demo app');
+
+var gui = window.require('nw.gui');
+
+var mb = new gui.Menu({type:'menubar'});
+mb.createMacBuiltin('Aviary Photo Editor');
+gui.Window.get().menu = mb;
