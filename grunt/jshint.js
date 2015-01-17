0d6cc22543e89f2e720de58b5c3f11ffc54fddee
jan4dev <jan4dev@gmail.com>
2015-01-17 22:49:17 +0100

    Finalized jshint configuration. Added a "use strict"; banner in all js files.

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -3,38 +3,62 @@ var files = [{
 }];
 
 module.exports = {
+
     options: {
+
         reporter: require('jshint-stylish'),
 
-        'maxerr'        : 50,       // {int} Maximum error before stopping
-
         // Enforcing
-        'newcap'        : true,     // true: Require capitalization of all constructor functions e.g. `new F()`
-        'quotmark'      : 'single', // Quotation mark consistency:
-        'maxdepth'      : 5,        // {int} Max depth of nested blocks (within functions)
-        'maxcomplexity' : 12,       // {int} Max cyclomatic complexity per function
-        'maxlen'        : 160,      // {int} Max number of characters per line
+        quotmark      : 'single', // Quotation mark consistency:
+        maxdepth      : 5,        // {int} Max depth of nested blocks (within functions)
+        maxcomplexity : 12,       // {int} Max cyclomatic complexity per function
+        maxlen        : 160,      // {int} Max number of characters per line
+        indent        : 4,
+        newcap        : true,    // true: Require capitalization of all constructor functions e.g. `new F()`
 
         // Relaxing
-        'eqnull'        : true,      // true: Tolerate use of `== null`
-        'es5'           : true,      // true: Allow ES5 syntax (ex: getters and setters)
-                                     // (ex: `for each`, multiple try/catch, function expression…)
-        'globalstrict'  : true,      // true: Allow global 'use strict' (also enables 'strict')
+        eqnull        : true,     // true: Tolerate use of `== null`
+        asi           : false,    // Ask for semicolons
+        debug         : false,    // true: Allow debugger statements e.g. browser breakpoints.
+        evil          : false,    // eval not allowed
+        proto         : false,    // __proto__ forbidden
+        scripturl     : false,    // urls like javascript:... forbidden
+        laxbreak      : false,
+        moz           : false,
+        noyield       : false,
+        strict        : false,     // true: Requires all functions run in ES5 Strict Mode
+        loopfunc      : false,
+        expr          : false,
+        es5           : false,     // true: Allow ES5 syntax (ex: getters and setters)
 
         // Environments
-        'browser'       : true,     // Web Browser (window, document, etc)
-        'devel'         : false,     // Development/debugging (alert, confirm, etc)
-        'jquery'        : true,    // jQuery
-        'mocha'         : true,     // Mocha
-        'node'          : true,    // Node.js
+        browser       : true,     // Web Browser (window, document, etc)
+        devel         : false,     // Development/debugging (alert, confirm, etc)
+        jquery        : true,
+        mocha         : true,
+        node          : true,
+        mootools      : false,
+        couch         : false,
+        jasmine       : false,
+        qunit         : false,
+        rhino         : false,
+        shelljs       : false,
+        prototypejs   : false,
+        yui           : false,
+        wsh           : false,
+        worker        : false,
+        nonstandard   : false,
+        browserify    : false,
+        devel         : false,
+        dojo          : false,
 
         // Custom Globals
-        'globals'       : {}        // additional predefined global variables
+        globals : {}
     },
     dev:{
         options:{
-            'debug'     : true,     // true: Allow debugger statements e.g. browser breakpoints.
-            'devel'     : true     // Development/debugging (alert, confirm, etc)
+            debug     : true,     // true: Allow debugger statements e.g. browser breakpoints.
+            devel     : true      // Development/debugging (alert, confirm, etc)
         },
         files:files
     },
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,36 +1,37 @@
 var files = [{
-    src: '**/*.js',
-    dest: 'build/',
-    expand: true,
+    src:'**/*.js',
+    dest:'build/',
+    expand:true,
     cwd:'src'
 }];
 
 module.exports = {
-    options: {
-        mangle: true,
-        compress: true,
-        beautify: false,
-        report: 'false'
+    options:{
+        mangle:true,
+        compress:true,
+        beautify:false,
+        report:'false',
+        banner:'"use strict";'
     },
-    dev: {
-        options: {
-            mangle: false,
-            beautify: {
-                beautify: true,
-                bracketize: true
+    dev:{
+        options:{
+            mangle:false,
+            beautify:{
+                beautify:true,
+                bracketize:true
             },
-            preserveComments: 'all',
-            compress: {
-                drop_debugger: false,
-                drop_console: false,
-                sequences: false,
-                join_vars: false
+            preserveComments:'all',
+            compress:{
+                drop_debugger:false,
+                drop_console:false,
+                sequences:false,
+                join_vars:false
             }
         },
-        files: files
+        files:files
     },
-    dist: {
-        files: files
+    dist:{
+        files:files
     }
 
 };
