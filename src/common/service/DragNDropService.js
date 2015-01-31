391cf829d0c5ca1d64f0365ca7cacde9802c538e
jan4dev <jan4dev@gmail.com>
2015-01-31 12:47:19 +0100

    Added possibility to import a url. Added an import view.

--- src/common/service/DragNDropService.js
+++ src/common/service/DragNDropService.js
@@ -9,6 +9,7 @@ var DragNDropService = Base.extend({
     initialize: function () {
         _.bindAll(this, 'onDrop', 'onDragOver');
     },
+
     // Dragging a file over the window will trigger an import view which will import the file in the app.
     listen: function () {
         nativeOnDragOver = win.ondragover;
@@ -17,24 +18,47 @@ var DragNDropService = Base.extend({
         win.ondragover = this.onDragOver;
         win.ondrop = this.onDrop;
     },
+
     // Restore the native behavior
     stopListening: function () {
         win.ondragover = nativeOnDragOver;
         win.ondrop = nativeOnDrop;
     },
+
     // When the user release the drag in the window, we can analyse what was dragged and eventually render the import view to treat it.
     onDrop: function (event) {
+
+        // We don't want the navigator to replace the content of the page with the dragged content
         event.preventDefault();
-        console.debug('dataTransfer: %o', event.dataTransfer);
+
+        // We accepts only two types of items : url (text/uri-list) and files
+
+        // Process urls
+        if(_.contains(event.dataTransfer.types, DragNDropService.URI_TYPE)){
+            this.processUrl(event.dataTransfer.getData(DragNDropService.URI_TYPE));
+        }
+
+        // Treating only the first file if there is several
         var files = event.dataTransfer.files;
         if(files && files.length){
-            var file = files[0].path;
-            console.info('Importing file: ', file);
-            var view = new ImportView({file:file});
-            $body.append(view.render().el);
-            // TODO  remove the window when hidden
+            this.processFile(files[0].path);
         }
     },
+
+    processUrl: function (url) {
+        console.info('Importing url: %s', url);
+        var view = new ImportView({url:url});
+        $body.append(view.render().el);
+        // TODO  remove the window when hidden
+    },
+
+    processFile: function (file) {
+        console.info('Importing file: ', file);
+        var view = new ImportView({file:file});
+        $body.append(view.render().el);
+        // TODO  remove the window when hidden
+    },
+
     // No op on drag over
     onDragOver: function (event) {
         event.preventDefault();
@@ -45,7 +69,9 @@ var DragNDropService = Base.extend({
             singleton = new DragNDropService();
         }
         return singleton;
-    }
+    },
+
+    URI_TYPE: 'text/uri-list'
 });
 
 module.exports = DragNDropService;
--- src/import/ImportView.js
+++ src/import/ImportView.js
@@ -1,6 +1,12 @@
 
+var template = JST['import/import']();
+
 var ImportView = Backbone.View.extend({
-    id:'import'
+    id:'import',
+    render: function () {
+        this.$el.html(template);
+        return this;
+    }
 });
 
 module.exports = ImportView;
